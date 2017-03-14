eb85b5fb35e4c1c1bb01481c5e252a88a7288f6b
Antonin Cezard <anc@ecedi.fr>
2017-03-14 18:29:40 +0100

    Section scaffolding

---
+++ src/components/Section/Section.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './Section.css'
+
+export class Section extends Component {
+  render () {
+    return (
+      <main>
+        <div className="container">
+          <div className="row">
+            <div className="col s12 m9 l10">
+            </div>
+          </div>
+        </div>
+      </main>
+    )
+  }
+}
+
+export default Section
